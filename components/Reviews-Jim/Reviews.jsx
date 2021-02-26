1afa5656f7fd44dee88536a9c110879ae31e1ede
Jim Burch <jrburch1@gmail.com>
2021-02-26 13:25:24 -0700

    added hello API for reviews

--- components/Reviews-Jim/Reviews.jsx
+++ components/Reviews-Jim/Reviews.jsx
@@ -1,9 +1,42 @@
-import React from 'react';
+/* eslint-disable */
 
-const Reviews = () => (
-  <div>
-    Reviews Widget
-  </div>
-);
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import config from '../../config';
+
+const Reviews = () => {
+  const [ reviews, setReviews ] = useState([]);
+
+  const getAllReviews = (query) => {
+    const options = {
+      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/reviews/?${query}`,
+      method: 'get',
+      headers: {
+        Authorization: config.TOKEN,
+      },
+    };
+
+    axios(options)
+      .then((res) => {
+        setReviews(res.data.results);
+      })
+      .catch((err) => { console.log(err); });
+  };
+
+  useEffect(() => {
+    getAllReviews('product_id=18201');
+  }, []);
+
+  return (
+    <div>
+      <h1>Reviews</h1>
+      {reviews.map((review) =>
+      <div key={review.review_id}>
+        <h3>{review.summary}</h3>
+        <p>{review.body}</p>
+      </div>)}
+    </div>
+  );
+};
 
 export default Reviews;
