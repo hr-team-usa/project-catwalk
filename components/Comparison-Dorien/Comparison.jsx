a906212cb49d42d10236d267cc75fe665b87d797
Dorien Pine <dorien.pine@gmail.com>
2021-02-26 16:08:44 -0700

    started framework for comparison

--- components/Comparison-Dorien/Comparison.jsx
+++ components/Comparison-Dorien/Comparison.jsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import config from "../../config";
+const Comparison = () => {
 
-const Comparison = () => (
-  <div>
-    Comparison Widget
-  </div>
-);
+  const [products, setProducts] = useState([]);
+
+  const getProducts = () => {
+
+    let options = {
+      method: 'get',
+      url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/products',
+      headers: {
+        'Authorization': config.TOKEN
+      }
+    }
+    axios(options)
+      .then((result) => {
+        setProducts(result.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  };
+
+  useEffect(() => {
+    getProducts()
+  }, []);
+
+
+
+  return (
+    <div>
+      Comparison Widget
+      {console.log(products)}
+      
+      {products.map((item, index) => (<div key={index}>*{item.category}* {item.name}</div>))}
+    </div>
+  )
+};
 
 export default Comparison;
