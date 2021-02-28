d7392248c99178f8ec36b2807e7dec2f5818e086
Scott Guinn <guinnscott111@gmail.com>
2021-02-27 18:48:35 -0700

    working carousel and mini-carousel with 7 thumbnails. Click thumbnail to update main image and shift.

--- components/Description-Scott/ProductDescription.jsx
+++ components/Description-Scott/ProductDescription.jsx
@@ -2,13 +2,24 @@
 import React from 'react';
 import ImageGallery from './ImageGallery.jsx';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from 'react-bootstrap';
+
 const ProductDescription = () => (
   // should receive productId state as a prop from index
   <div>
-    <ImageGallery />
-    <div> Product Info Component will go here </div>
-    <div> Style Selector will go here </div>
-    <div> Add to Cart will go here </div>
+    <Container>
+      <Row>
+        <Col>
+          <ImageGallery />
+        </Col>
+        <Col>
+          <div> Product Info Component will go here </div>
+          <div> Style Selector will go here </div>
+          <div> Add to Cart will go here </div>
+        </Col>
+      </Row>
+    </Container>
   </div>
 );
 
