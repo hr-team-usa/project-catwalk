489d0951c1169aeaaeacaf5edaf82ff18f07dcc2
Scott Guinn <guinnscott111@gmail.com>
2021-03-01 10:48:37 -0700

    Skeleton of Product Info Subwidget added. Social Media buttons added (limited functionality currently)

--- components/Description-Scott/ProductDescription.jsx
+++ components/Description-Scott/ProductDescription.jsx
@@ -1,12 +1,40 @@
 /* eslint-disable */
-import React from 'react';
-import ImageGallery from './ImageGallery.jsx';
+import React, {useState, useEffect} from 'react';
+import ImageGallery from './ImageGallery/ImageGallery.jsx';
+import ProductInfo from './ProductInfo/ProductInfo.jsx';
+import axios from 'axios';
+import config from '../../config';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from 'react-bootstrap';
 
-const ProductDescription = () => (
+const ProductDescription = () => {
+  var productId = 18078;
+  var [productName, setProductName] = useState('');
+  var [category, setCategory] = useState('');
+  var [description, setDescription] = useState('');
+
+  const getProduct = () => {
+    const options = {
+      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/products/${productId}`,
+      method: 'get',
+      headers: {
+        Authorization: config.TOKEN,
+      }
+    }
+    axios(options)
+    .then((results)=> {
+      console.log('product results.data: ', results.data);
+      setProductName(results.data.name);
+      setCategory(results.data.category);
+      setDescription(results.data.description);
+    }).catch((err) => console.error(err));
+  }
+
+  useEffect(()=> {
+    getProduct();
+  }, []);
   // should receive productId state as a prop from index
+  return (
   <div>
     <Container>
       <Row>
@@ -14,13 +42,16 @@ const ProductDescription = () => (
           <ImageGallery />
         </Col>
         <Col>
-          <div> Product Info Component will go here </div>
+          <ProductInfo productName={productName}
+                       category={category}
+                       description={description}/>
           <div> Style Selector will go here </div>
           <div> Add to Cart will go here </div>
         </Col>
       </Row>
     </Container>
   </div>
-);
+  )
+};
 
 export default ProductDescription;
