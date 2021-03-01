b96eae7bbcf3de5fa06a42b08b6912a407e9f80f
Jim Burch <jrburch1@gmail.com>
2021-03-01 16:53:03 -0700

    tried sass on stars

--- components/Reviews-Jim/components/Stars.jsx
+++ components/Reviews-Jim/components/Stars.jsx
@@ -1,12 +1,13 @@
 /* eslint-disable */
 
 import React, { useState, useEffect } from 'react';
+// import styles from './Stars.module.scss';
 
-const Stars = () => {
+const Stars = props => {
 
   return (
     <div>
-      <div>rating stars</div>
+      {/* <div className="Stars" style="--rating: 2.3;" aria-label="Rating of this product is 2.3 out of 5."></div> */}
     </div>
   );
 };
--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
+const path = require('path');
+
 module.exports = {
   images: {
     domains: ["images.unsplash.com"],
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
   },
 };
