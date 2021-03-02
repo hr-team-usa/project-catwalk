bec72e09656b92fb8f8b2d9854cd65078e49491f
Jim Burch <jrburch1@gmail.com>
2021-03-01 17:41:08 -0700

    finished stars component

--- components/Reviews-Jim/components/Stars.jsx
+++ components/Reviews-Jim/components/Stars.jsx
@@ -1,15 +1,17 @@
 /* eslint-disable */
 
 import React, { useState, useEffect } from 'react';
-// import styles from './Stars.module.scss';
+import Rating from '@material-ui/lab/Rating';
 
 const Stars = props => {
 
   return (
     <div>
-      {/* <div className="Stars" style="--rating: 2.3;" aria-label="Rating of this product is 2.3 out of 5."></div> */}
+      <Rating className="star-rating" value={Number(props.rating)} precision={0.25} readOnly />
     </div>
   );
 };
 
 export default Stars;
+
+// https://material-ui.com/components/rating/
--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
-const path = require('path');
-
 module.exports = {
   images: {
     domains: ["images.unsplash.com"],
-  },
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
   },
 };
