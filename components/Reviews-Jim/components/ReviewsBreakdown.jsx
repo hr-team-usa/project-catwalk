4f51ee9ec309885f792514d5e832f51460fb67b5
Jim Burch <jrburch1@gmail.com>
2021-03-01 20:08:09 -0700

    finished percentage recommended

--- components/Reviews-Jim/components/ReviewsBreakdown.jsx
+++ components/Reviews-Jim/components/ReviewsBreakdown.jsx
@@ -8,50 +8,63 @@ import Stars from './Stars'
 import config from '../../../config';
 
 const ReviewsBreakdown = ({ productId }) => {
-
+  const [ productMeta, setProductMeta ] = useState(null);
   const [ rating, setRating ] = useState(null);
+  const [ recommended, setRecommended ] = useState(null);
 
-  const getAvgRating = (product) => {
+  const getProductMeta = (productId) => {
     const api = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/reviews/meta';
     const options = {
-      url: `${api}?product_id=${product}`,
+      url: `${api}?product_id=${productId}`,
       method: 'get',
       headers: {
         Authorization: config.TOKEN,
       },
     };
 
-    const ratingCreator = (ratingsObj) => {
-      let allRatings = 0;
-      let ratingCount = 0;
-      for (let key in ratingsObj) {
-        for (let i = 0; i < ratingsObj[key]; i++) {
-          allRatings += Number(key);
-          ratingCount++;
-        }
-      }
-      return (allRatings / ratingCount).toFixed(1);
-    }
-
     axios(options)
       .then((res) => {
-        return ratingCreator(res.data.ratings);
-      })
-      .then((result) => {
-        setRating(result)
+        setProductMeta(res.data)
       })
       .catch((err) => { console.log(err); });
+  };
+
+  const ratingCreator = (ratingsObj) => {
+    let allRatings = 0;
+    let ratingCount = 0;
+    for (let key in ratingsObj) {
+      for (let i = 0; i < ratingsObj[key]; i++) {
+        allRatings += Number(key);
+        ratingCount++;
+      }
+    }
+    return setRating((allRatings / ratingCount).toFixed(1));
+  };
+
+  const reviewPercentage = (ratings, recommended) => {
+    let totalRatings = 0;
+    for (let key in ratings) {
+      totalRatings += Number(ratings[key]);
+    }
+    return setRecommended(Math.floor((Number(recommended) / totalRatings) * 100));
   }
 
   useEffect(() => {
-    getAvgRating(productId);
-  })
+    getProductMeta(productId);
+  }, []);
+
+  useEffect(() => {
+    if (productMeta) {
+      ratingCreator(productMeta.ratings);
+      reviewPercentage(productMeta.ratings, productMeta.recommended.true);
+    }
+  }, [productMeta])
 
   return (
     <div>
-      <div>average-rating: <h4>{rating}</h4></div>
+      <div><h4>{rating}</h4></div>
       <Stars rating={rating} />
-      <div>percent recommended</div>
+      <div>{recommended}% of reviews recommend this product</div>
       <RatingBreakdown />
       <ProductBreakdown />
     </div>
