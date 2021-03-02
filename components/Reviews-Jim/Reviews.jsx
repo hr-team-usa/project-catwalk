7e6e439ff041d499e83c59cdfaf91a382b517831
Jim Burch <jrburch1@gmail.com>
2021-03-02 09:42:14 -0700

    reviews cleaned up

--- components/Reviews-Jim/Reviews.jsx
+++ components/Reviews-Jim/Reviews.jsx
@@ -1,23 +1,18 @@
-/* eslint-disable */
-
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import ReviewsList from './components/ReviewsList';
 import ReviewsBreakdown from './components/ReviewsBreakdown';
 
-const Reviews = ({ productId }) => {
-
-  return (
-    <Container>
-      <h3>Ratings & Reviews</h3>
-      <Row>
-        <Col xs={4}><ReviewsBreakdown productId={productId} /></Col>
-        <Col><ReviewsList productId={productId} /></Col>
-      </Row>
-    </Container>
-  );
-};
+const Reviews = ({ productId }) => (
+  <Container>
+    <h3>Ratings & Reviews</h3>
+    <Row>
+      <Col xs={4}><ReviewsBreakdown productId={productId} /></Col>
+      <Col><ReviewsList productId={productId} /></Col>
+    </Row>
+  </Container>
+);
 
 export default Reviews;
