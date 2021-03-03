d248acaf07974410b5872d28e141b3c37fbf2c08
Jim Burch <jrburch1@gmail.com>
2021-03-03 14:12:47 -0700

    added ratings bars

--- components/Reviews-Jim/components/RatingBreakdown.jsx
+++ components/Reviews-Jim/components/RatingBreakdown.jsx
@@ -1,15 +1,61 @@
 /* eslint-disable */
 import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import LinearProgress from '@material-ui/core/LinearProgress';
 
-const RatingBreakdown = () => {
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+  },
+});
+
+const RatingBreakdown = ({ ratings }) => {
+  const classes = useStyles();
+  const [percentages, setPercentages] = useState({});
+
+  const createBarRatings = (obj) => {
+    const keys = Object.keys(obj).map(Number);
+    const values = Object.values(obj).map(Number);
+    const ratingsTotal = Object.values(obj).map(Number).reduce((a, b) => (a + b), 0);
+    let ratingsObj = {
+      5: 0,
+      4: 0,
+      3: 0,
+      2: 0,
+      1: 0,
+    }
+    for (let i = 0; i < keys.length; i += 1) {
+      ratingsObj[keys[i]] = findPercentage(values[i], ratingsTotal);
+    }
+    console.log('ratings object', ratingsObj)
+    return setPercentages(ratingsObj);
+  };
+
+  const findPercentage = (rating, total) => {
+    return Math.ceil((rating / total) * 100);
+  };
+
+  useEffect(() => {
+    createBarRatings(ratings);
+  }, []);
 
   return (
-    <div>
-      <div>5-stars-bar</div>
-      <div>4-stars-bars</div>
-      <div>3-stars-bar</div>
-      <div>2-stars-bar</div>
-      <div>1-star-bar</div>
+    <div className={classes.root}>
+      <div>5 Stars
+        <LinearProgress variant="determinate" value={percentages['5']} />
+      </div>
+      <div>4 Stars
+        <LinearProgress variant="determinate" value={percentages['4']} />
+      </div>
+      <div>3 Stars
+        <LinearProgress variant="determinate" value={percentages['3']} />
+      </div>
+      <div>2 Stars
+        <LinearProgress variant="determinate" value={percentages['2']} />
+      </div>
+      <div>1 Stars
+        <LinearProgress variant="determinate" value={percentages['1']} />
+      </div>
     </div>
   );
 };
