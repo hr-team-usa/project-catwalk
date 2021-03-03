ed917d8639f8ee4b4805b1453f4e1cbea96f7109
Jim Burch <jrburch1@gmail.com>
2021-03-03 14:45:42 -0700

    added ratings bars and cleaned linter

--- components/Reviews-Jim/components/RatingBreakdown.jsx
+++ components/Reviews-Jim/components/RatingBreakdown.jsx
@@ -1,5 +1,5 @@
-/* eslint-disable */
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import LinearProgress from '@material-ui/core/LinearProgress';
 
@@ -12,27 +12,23 @@ const useStyles = makeStyles({
 const RatingBreakdown = ({ ratings }) => {
   const classes = useStyles();
   const [percentages, setPercentages] = useState({});
+  const findPercentage = (rating, total) => Math.floor((rating / total) * 100);
 
   const createBarRatings = (obj) => {
     const keys = Object.keys(obj).map(Number);
     const values = Object.values(obj).map(Number);
     const ratingsTotal = Object.values(obj).map(Number).reduce((a, b) => (a + b), 0);
-    let ratingsObj = {
+    const ratingsObj = {
       5: 0,
       4: 0,
       3: 0,
       2: 0,
       1: 0,
-    }
+    };
     for (let i = 0; i < keys.length; i += 1) {
       ratingsObj[keys[i]] = findPercentage(values[i], ratingsTotal);
     }
-    console.log('ratings object', ratingsObj)
-    return setPercentages(ratingsObj);
-  };
-
-  const findPercentage = (rating, total) => {
-    return Math.ceil((rating / total) * 100);
+    setPercentages(ratingsObj);
   };
 
   useEffect(() => {
@@ -41,23 +37,42 @@ const RatingBreakdown = ({ ratings }) => {
 
   return (
     <div className={classes.root}>
-      <div>5 Stars
-        <LinearProgress variant="determinate" value={percentages['5']} />
+      <div>
+        5 Stars
+        <LinearProgress variant="determinate" value={Number(percentages['5'])} />
       </div>
-      <div>4 Stars
-        <LinearProgress variant="determinate" value={percentages['4']} />
+      <div>
+        4 Stars
+        <LinearProgress variant="determinate" value={Number(percentages['4'])} />
       </div>
-      <div>3 Stars
-        <LinearProgress variant="determinate" value={percentages['3']} />
+      <div>
+        3 Stars
+        <LinearProgress variant="determinate" value={Number(percentages['3'])} />
       </div>
-      <div>2 Stars
-        <LinearProgress variant="determinate" value={percentages['2']} />
+      <div>
+        2 Stars
+        <LinearProgress variant="determinate" value={Number(percentages['2'])} />
       </div>
-      <div>1 Stars
-        <LinearProgress variant="determinate" value={percentages['1']} />
+      <div>
+        1 Stars
+        <LinearProgress variant="determinate" value={Number(percentages['1'])} />
       </div>
     </div>
   );
+};
+
+RatingBreakdown.propTypes = {
+  ratings: PropTypes.shape({
+    1: PropTypes.string,
+    2: PropTypes.string,
+    3: PropTypes.string,
+    4: PropTypes.string,
+    5: PropTypes.string,
+  }),
+};
+
+RatingBreakdown.defaultProps = {
+  ratings: null,
 };
 
 export default RatingBreakdown;
