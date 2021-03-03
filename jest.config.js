22ca46a1be2123873ebcab44b77169743b822492
Scott Guinn <guinnscott111@gmail.com>
2021-03-03 10:58:14 -0700

    description.spec.js now has two annotated useful and working tests.

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   setupFiles: ['<rootDir>/jest.setup.js'],
+  verbose: true,
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   moduleNameMapper: {
     "\\.css$": "identity-obj-proxy",
