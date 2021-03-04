d8fa0d86d805bbc9cda288cf65d5e5d0838d0214
Malcolm Marshall <m.marshall369@gmail.com>
2021-03-03 19:48:02 -0700

    fixed test error

--- components/QAs-Malcolm/components/Questions.jsx
+++ components/QAs-Malcolm/components/Questions.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import config from '../../../config';
 import Q from './Q';
 
+// eslint-disable-next-line no-unused-vars
 function Questions({ productId }) {
   const [questions, setQuestions] = useState([]);
   const id = productId;
