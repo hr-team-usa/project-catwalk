c03c3f10e7c3f9f494da2176856a2838908db645
Malcolm Marshall <m.marshall369@gmail.com>
2021-03-04 10:00:49 -0700

    Answers sorted in order of helpfulness

--- components/QAs-Malcolm/components/Questions.jsx
+++ components/QAs-Malcolm/components/Questions.jsx
@@ -7,11 +7,11 @@ import Q from './Q';
 // eslint-disable-next-line no-unused-vars
 function Questions({ productId }) {
   const [questions, setQuestions] = useState([]);
-  const id = 18078;
+  const id = 18201;
 
   const getQuestions = () => {
     const options = {
-      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/qa/questions?product_id=${id}`,
+      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/qa/questions?product_id=${id}&count=4`,
 
       method: 'get',
       headers: {
