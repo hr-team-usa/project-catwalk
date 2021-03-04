c4c5917f7550c2c0478dc120cd8d04128400d51d
Scott Guinn <guinnscott111@gmail.com>
2021-03-04 15:37:15 -0700

    pass down and invoke setProductRating

--- components/Reviews-Jim/Reviews.jsx
+++ components/Reviews-Jim/Reviews.jsx
@@ -6,11 +6,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import ReviewsList from './components/ReviewsList';
 import ReviewsBreakdown from './components/ReviewsBreakdown';
 
-const Reviews = ({ productId }) => (
+const Reviews = ({ productId, setProductRating }) => (
   <Container>
     <h3>Ratings & Reviews</h3>
     <Row>
-      <Col xs={4}><ReviewsBreakdown productId={productId} /></Col>
+      <Col xs={4}>
+        <ReviewsBreakdown
+          productId={productId}
+          setProductRating={setProductRating}
+        />
+      </Col>
       <Col><ReviewsList productId={productId} /></Col>
     </Row>
   </Container>
