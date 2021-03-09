0ad47ea62754138ca03c01f1a21477162276990d
Dorien Pine <dorien.pine@gmail.com>
2021-03-08 21:00:30 -0700

    eslinter fix

--- components/Comparison-Dorien/Comparison.jsx
+++ components/Comparison-Dorien/Comparison.jsx
@@ -1,26 +1,24 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import config from "../../config";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import config from '../../config';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import RelatedProducts from "./RelatedProducts";
-import OutfitList from "./OutfitList";
-import { Container } from "react-bootstrap";
+import RelatedProducts from './RelatedProducts';
+import OutfitList from './OutfitList';
+import { Container } from 'react-bootstrap';
 
-
-
-const Comparison = ({productId, setProductId}) => {
+const Comparison = ({ productId, setProductId }) => {
   const [products, setProducts] = useState([]);
   const [productImg, setProductImg] = useState(false);
   const [productStyle, setProductStyle] = useState(false);
 
   const getRelatedProductsId = () => {
-    let options = {
+    const options = {
       method: 'get',
       url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/products/${productId}/related`,
       headers: {
-        'Authorization': config.TOKEN
-      }
-    }
+        Authorization: config.TOKEN,
+      },
+    };
     axios(options)
       .then((result) => {
         if (result.data.length !== 0) {
@@ -30,63 +28,63 @@ const Comparison = ({productId, setProductId}) => {
       })
       .catch((err) => {
         console.log(err);
-      })
-  }
+      });
+  };
 
   const getRelatedProducts = (productIds) => {
-    let arr = [];
-    productIds.map(product => {
-      arr.push(axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/products/${product}`, {headers: {Authorization: config.TOKEN}}))
-      });
-      // console.log('current Array: ', arr);
-      Promise.all(arr)
-      .then(results => {
+    const arr = [];
+    productIds.map((product) => {
+      arr.push(axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/products/${product}`, { headers: { Authorization: config.TOKEN } }));
+    });
+    // console.log('current Array: ', arr);
+    Promise.all(arr)
+      .then((results) => {
         setProducts(results);
       })
       .catch((err) => {
-        console.log(err)
-      })
-  }
+        console.log(err);
+      });
+  };
 
   const getRelatedImages = (productIds) => { // adding product price since the price and images are from the same endpoint
-    let obj = {};
-    let objStyle = {};
-    let arr = [];
-    productIds.map(product => {
-      arr.push(axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/products/${product}/styles`, {headers: {Authorization: config.TOKEN}}))
-      });
-      Promise.all(arr)
-      .then(results => {
+    const obj = {};
+    const objStyle = {};
+    const arr = [];
+    productIds.map((product) => {
+      arr.push(axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/products/${product}/styles`, { headers: { Authorization: config.TOKEN } }));
+    });
+    Promise.all(arr)
+      .then((results) => {
         results.map((item) => {
           if (item.data.results[0].photos[0].thumbnail_url) {
-            obj[item.data.product_id] = item.data.results[0].photos[0].thumbnail_url
+            obj[item.data.product_id] = item.data.results[0].photos[0].thumbnail_url;
           } else {
-            obj[item.data.product_id] = 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22263%22%20height%3D%22160%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20263%20160%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_177e4e6d2c3%20text%20%7B%20fill%3A%23999%3Bfont-weight%3Anormal%3Bfont-family%3A-apple-system%2CBlinkMacSystemFont%2C%26quot%3BSegoe%20UI%26quot%3B%2CRoboto%2C%26quot%3BHelvetica%20Neue%26quot%3B%2CArial%2C%26quot%3BNoto%20Sans%26quot%3B%2C%26quot%3BLiberation%20Sans%26quot%3B%2Csans-serif%2C%26quot%3BApple%20Color%20Emoji%26quot%3B%2C%26quot%3BSegoe%20UI%20Emoji%26quot%3B%2C%26quot%3BSegoe%20UI%20Symbol%26quot%3B%2C%26quot%3BNoto%20Color%20Emoji%26quot%3B%2C%20monospace%3Bfont-size%3A13pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_177e4e6d2c3%22%3E%3Crect%20width%3D%22263%22%20height%3D%22160%22%20fill%3D%22%23373940%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2297.25%22%20y%3D%2286.15%22%3E263x160%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E'
+            obj[item.data.product_id] = 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22263%22%20height%3D%22160%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20263%20160%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_177e4e6d2c3%20text%20%7B%20fill%3A%23999%3Bfont-weight%3Anormal%3Bfont-family%3A-apple-system%2CBlinkMacSystemFont%2C%26quot%3BSegoe%20UI%26quot%3B%2CRoboto%2C%26quot%3BHelvetica%20Neue%26quot%3B%2CArial%2C%26quot%3BNoto%20Sans%26quot%3B%2C%26quot%3BLiberation%20Sans%26quot%3B%2Csans-serif%2C%26quot%3BApple%20Color%20Emoji%26quot%3B%2C%26quot%3BSegoe%20UI%20Emoji%26quot%3B%2C%26quot%3BSegoe%20UI%20Symbol%26quot%3B%2C%26quot%3BNoto%20Color%20Emoji%26quot%3B%2C%20monospace%3Bfont-size%3A13pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_177e4e6d2c3%22%3E%3Crect%20width%3D%22263%22%20height%3D%22160%22%20fill%3D%22%23373940%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2297.25%22%20y%3D%2286.15%22%3E263x160%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E';
           }
           objStyle[item.data.product_id] = item.data.results[0];
-        })
+        });
         setProductImg(obj);
         setProductStyle(objStyle);
       })
       .catch((err) => {
-        console.log(err)
-      })
-  }
+        console.log(err);
+      });
+  };
 
   useEffect(() => {
-    getRelatedProductsId()
+    getRelatedProductsId();
   }, [productId]);
 
   return (
     <div>
       <Container>
-      {products.length > 0 && productImg && productStyle ?
-          <RelatedProducts products={products} images={productImg} style={productStyle} setProductId={setProductId} setProducts={setProducts} setProductImg={setProductImg} setProductStyle={setProductStyle}/>
+        {products.length > 0 && productImg && productStyle
+          ? <RelatedProducts products={products} images={productImg} style={productStyle} setProductId={setProductId} setProducts={setProducts} setProductImg={setProductImg} setProductStyle={setProductStyle} />
           : null }
-          <OutfitList />
+        <OutfitList />
       </Container>
     </div>
-  )
+  );
 };
 
 export default Comparison;
--- components/Comparison-Dorien/RelatedProducts.jsx
+++ components/Comparison-Dorien/RelatedProducts.jsx
@@ -1,115 +1,120 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import Card from 'react-bootstrap/Card';
 import CardDeck from 'react-bootstrap/CardDeck';
 import Carousel from 'react-bootstrap/Carousel';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import styles from './price.module.css';
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
-import Comparison_Modal from "./Comparison-Modal"
+import Comparison_Modal from './Comparison-Modal';
 
+const RelatedProducts = ({
+  products, images, style, setProductId, setProducts, setProductImg, setProductStyle,
+}) => {
+  // in order to make multiple slides in carousel
+  const arrayOfArrayProducts = [];
+  let arrayOfProducts = [];
+  const copyOfProducts = products.slice();
 
-const RelatedProducts = ({ products, images, style, setProductId, setProducts, setProductImg, setProductStyle }) => {
-
-    // in order to make multiple slides in carousel
-    let arrayOfArrayProducts = [];
-    let arrayOfProducts = [];
-    let copyOfProducts = products.slice();
-
-    if (products.length === 0) {
-        products = [
-            {
-                data: {
-                    id: 18079,
-                    name: "null",
-                    category: "null"
-                }
-            },
-            {
-                data: {
-                    id: 18079,
-                    name: "null",
-                    category: "null"
-                }
-            },
-            {
-                data: {
-                    id: 18079,
-                    name: "null",
-                    category: "null"
-                }
-            },
-            {
-                data: {
-                    id: 18079,
-                    name: "null",
-                    category: "null"
-                }
-            },
-        ];
+  if (products.length === 0) {
+    products = [
+      {
+        data: {
+          id: 18079,
+          name: 'null',
+          category: 'null',
+        },
+      },
+      {
+        data: {
+          id: 18079,
+          name: 'null',
+          category: 'null',
+        },
+      },
+      {
+        data: {
+          id: 18079,
+          name: 'null',
+          category: 'null',
+        },
+      },
+      {
+        data: {
+          id: 18079,
+          name: 'null',
+          category: 'null',
+        },
+      },
+    ];
+  }
+  for (let i = 0; i <= Math.ceil(copyOfProducts.length / 4); i++) {
+    arrayOfProducts = copyOfProducts.splice(0, 4);
+    if (arrayOfProducts.length < 4) {
+      while (arrayOfProducts.length < 4) {
+        arrayOfProducts.push({
+          data: {
+            id: 18079,
+            name: 'Out of stock',
+            category: 'null',
+          },
+        });
+      }
     }
-    for (let i = 0; i <= Math.ceil(copyOfProducts.length / 4); i++) {
-        arrayOfProducts = copyOfProducts.splice(0, 4);
-        if (arrayOfProducts.length < 4) {
-            while (arrayOfProducts.length < 4) {
-                arrayOfProducts.push({
-                    data: {
-                        id: 18079,
-                        name: "Out of stock",
-                        category: "null"
-                    }
-                })
-            }
-        }
-        arrayOfArrayProducts.push(arrayOfProducts);
-        arrayOfProducts = [];
-    }
+    arrayOfArrayProducts.push(arrayOfProducts);
+    arrayOfProducts = [];
+  }
 
-    let changeProduct = (itemId) => {
-        setProductId(itemId)
-        setProducts([])
-        setProductImg(false)
-        setProductStyle(false)
-    }
+  const changeProduct = (itemId) => {
+    setProductId(itemId);
+    setProducts([]);
+    setProductImg(false);
+    setProductStyle(false);
+  };
 
-    return (
-        <>
+  return (
+    <>
 
-            Related Products
-            {products &&
+      Related Products
+      {products
+                && (
                 <Carousel>
-                    {arrayOfArrayProducts.map((array, index) => (
-                        <Carousel.Item key={index}>
-                            <CardDeck>
-                                {array.map((item, index) => (
-                                    <OverlayTrigger
-                                    placement="right"
-                                    delay={{ show: 250, hide: 400 }}
-                                    overlay={Comparison_Modal}
-                                  >
-                                    <Card key={index} className="related-products" style={{ width: '5rem' }} onClick={() => changeProduct(item.data.id)}>
-                                        <Card.Img variant="top" className="related-image" src={images[item.data.id.toString()]} />
-                                        <Card.Title>{item.data.name}</Card.Title>
-                                        <Card.Subtitle className="mb-2 text-muted">{item.data.category}</Card.Subtitle>
-                                        {style &&
-                                            style[item.data.id.toString()].sale_price || style[item.data.id.toString()] !== undefined ? (
-                                                <Card.Text>
-                                                    <span className={styles.salePrice}>{style[item.data.id.toString()].sale_price}</span>
-                                                    <span className={styles.originalPrice}>{style[item.data.id.toString()].original_price}</span>
-                                                </Card.Text>
-                                            ) :
-                                            <Card.Text>
-                                                <span>{style[item.data.id.toString()].original_price}</span>
-                                            </Card.Text>
-                                        }
-                                    </Card>
-                                    </OverlayTrigger>
-                                ))}
-                            </CardDeck>
-                        </Carousel.Item>
-                    ))}
+                  {arrayOfArrayProducts.map((array, index) => (
+                    <Carousel.Item key={index}>
+                      <CardDeck>
+                        {array.map((item, index) => (
+                          <OverlayTrigger
+                            placement="right"
+                            delay={{ show: 250, hide: 400 }}
+                            overlay={Comparison_Modal}
+                          >
+                            <Card key={index} className="related-products" style={{ width: '5rem' }} onClick={() => changeProduct(item.data.id)}>
+                              <Card.Img variant="top" className="related-image" src={images[item.data.id.toString()]} />
+                              <Card.Title>{item.data.name}</Card.Title>
+                              <Card.Subtitle className="mb-2 text-muted">{item.data.category}</Card.Subtitle>
+                              {style && style[item.data.id.toString()].sale_price &&
+                              style[item.data.id.toString()] !== undefined ? (
+                                <Card.Text>
+                                  <span className={styles.salePrice}>
+                                    {style[item.data.id.toString()].sale_price}
+                                  </span>
+                                  <span className={styles.originalPrice}>
+                                    {style[item.data.id.toString()].original_price}
+                                  </span>
+                                </Card.Text>
+                                ) : (
+                                  <Card.Text>
+                                    <span>{style[item.data.id.toString()].original_price}</span>
+                                  </Card.Text>
+                                )}
+                            </Card>
+                          </OverlayTrigger>
+                        ))}
+                      </CardDeck>
+                    </Carousel.Item>
+                  ))}
                 </Carousel>
-            }
-            <style>
-                {`
+                )}
+      <style>
+        {`
            .related-image {
              height: 160px;
            }
@@ -125,9 +130,9 @@ const RelatedProducts = ({ products, images, style, setProductId, setProducts, s
             width: -1%;
             }
            `}
-            </style>
-        </>
-    )
-}
+      </style>
+    </>
+  );
+};
 
 export default RelatedProducts;
