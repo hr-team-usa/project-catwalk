1ed8561a971d51cd56e77273e0ce44d223e87e59
Scott Guinn <guinnscott111@gmail.com>
2021-03-09 21:56:28 -0700

    eslinting errors fixed. reverted SelectSize to display Overlay

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+/* eslint-disable */
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootswatch/dist/lux/bootstrap.min.css';
 // import 'bootswatch/dist/slate/bootstrap.min.css';
