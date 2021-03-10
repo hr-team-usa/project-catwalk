9e4912ecccf717d7af878290950a7e14f35b8d3f
Jim Burch <jrburch1@gmail.com>
2021-03-09 19:51:38 -0700

    global styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootswatch/dist/lux/bootstrap.min.css';
 import '../styles/styles.css';
 
 export default function App({ Component, pageProps }) {
