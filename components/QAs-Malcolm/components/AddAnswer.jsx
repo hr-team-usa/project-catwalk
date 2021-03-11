73f9c54702a0462084f7cd75a16a5a8251036d49
Malcolm Marshall <m.marshall369@gmail.com>
2021-03-11 16:28:11 -0700

    form validation complete

--- components/QAs-Malcolm/components/AddAnswer.jsx
+++ components/QAs-Malcolm/components/AddAnswer.jsx
@@ -31,33 +31,51 @@ function AddAnswer(props) {
     }
   };
 
-  const submitQ = () => {
-    const options = {
-      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/qa/questions/${props.questionId}/answers`,
-      method: 'post',
-      headers: {
-        Authorization: config.TOKEN,
-      },
-      data: {
-        body: answer,
-        name,
-        email,
-        photos: [],
-      },
-    };
-
-    axios(options)
-      .then(() => { props.onHide(); })
-      .then(() => console.log('created in db'))
-      .catch((err) => console.log(err));
+  const validationCheck = () => {
+    const required = [];
+    if (answer === '') {
+      required.push('answer');
+    }
+    if (name === '') {
+      required.push('nickname');
+    }
+    if (email === '') {
+      required.push('email address');
+    }
+    if (required.length) {
+      let result = '\n';
+      for (let i = 0; i < required.length; i += 1) {
+        result += `${required[i]}\n`;
+      }
+      return result;
+    }
+    return null;
   };
 
-  // const handleAddPhoto = (e) => {
-  //   console.log(e.target.parentNode.input);
-  //   // const file = e.target.files[0];
-  //   const reader = new FileReader();
-  //   const url = reader.readAsDataURL(file.name);
-  // };
+  const submitQ = (e) => {
+    e.preventDefault();
+    if (!validationCheck()) {
+      const options = {
+        url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/qa/questions/${props.questionId}/answers`,
+        method: 'post',
+        headers: {
+          Authorization: config.TOKEN,
+        },
+        data: {
+          body: answer,
+          name,
+          email,
+          photos: [],
+        },
+      };
+      axios(options)
+        .then(() => { props.onHide(); })
+        .then(() => console.log('created in db'))
+        .catch((err) => console.log(err));
+    } else {
+      alert(`Please complete the required fields: ${validationCheck()}`);
+    }
+  };
 
   return (
     <Modal {...props} aria-labelledby="contained-modal-title-vcenter" centered>
@@ -78,15 +96,19 @@ function AddAnswer(props) {
                   onChange={(e) => { handleChange(e); }}
                   required
                 >
-                  <Form.Label>Your Answer *</Form.Label>
+                  <Form.Label>Your Answer (required)</Form.Label>
                   <Form.Control
                     as="textarea"
                     rows={3}
                     name="formAnswer"
                   />
+                  <Form.Text>
+                    {answer.length}
+                    /1000 max
+                  </Form.Text>
                 </Form.Group>
                 <Form.Group onChange={(e) => { handleChange(e); }} required>
-                  <Form.Label>What is your nickname *</Form.Label>
+                  <Form.Label>What is your nickname (required)</Form.Label>
                   <Form.Control
                     type="name"
                     placeholder="Example: jack543!"
@@ -102,7 +124,7 @@ function AddAnswer(props) {
                   onChange={(e) => { handleChange(e); }}
                   required
                 >
-                  <Form.Label>Email address *</Form.Label>
+                  <Form.Label>Email address (required)</Form.Label>
                   <Form.Control
                     type="email"
                     placeholder="Example: jack@email.com"
@@ -131,7 +153,7 @@ function AddAnswer(props) {
                 </Form.Group>
                 <br />
                 <br />
-                <Button variant="primary" type="submit" onClick={() => submitQ()}>
+                <Button variant="primary" type="submit" onClick={(e) => submitQ(e)}>
                   Submit
                 </Button>
               </Form>
